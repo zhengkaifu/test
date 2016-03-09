fec054ec2d3922831cbca0f8ac1b156db56f2a4a
zhengkaifu@tju.edu.com <zhengkaifu@tju.edu.com>
2016-03-09 21:49:20 +0800

    asdd

--- test.py
+++ test.py
@@ -7,10 +7,14 @@ from skimage import transform, filters
 from augment import (img_augment, sample_img_augment_params, AugmentedInput,
                       SupervisedAugmentedInput)
 from util import img_transform
+import joblib
 
 def imgs(self):
     for i in range(self.n_imgs):
-        yield self.img(i)
+        yield self._img(i)
+def _img(self, idx):
+    img_path = os.path.join("./", '%.6d.jpg' % (idx+1))
+    return np.array(Image.open(img_path))
 def _resize(args):
     img, rescale_size, bbox = args
     img = img[bbox[0]:bbox[1], bbox[2]:bbox[3]]
@@ -35,10 +39,18 @@ def _resize_augment(args):
 
 if __name__ == '__main__':
 #	img=np.array(Image.open('./000001.jpg').convert('L'))
-	img=np.array(Image.open('./000001.jpg'))
+	# img=np.array(Image.open('./000001.jpg'))
 	# a.show()
 	# a.save('aa.jpg')
-	img = _resize_augment((img,64,(40, 218-30, 15, 178-15)))
-        image2 = Image.fromarray(img)
-	image2 = image2.convert('L')
-        image2.save('aa.jpg')
+	# img = _resize_augment((img,64,(40, 218-30, 15, 178-15)))
+ #        image2 = Image.fromarray(img)
+	# image2 = image2.convert('L')
+ #        image2.save('aa.jpg')
+    def img_iter():
+        for i in range(n_imgs):
+            yield _img(img_idxs[i % len(img_idxs)])
+    with joblib.Parallel(n_jobs=-2) as parallel:
+        imgs = parallel(joblib.delayed(_resize_augment)
+                        ((img, 64, (40, 218-30, 15, 178-15))) for img in img_iter())
+    imgs = np.array(imgs)
+    print imgs
