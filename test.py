d4bc01dbff460162f0ee919761fd31e1bf20955c
root <424107845@qq.com>
2016-03-09 21:41:20 +0800

    ss

--- test.py
+++ test.py
@@ -7,6 +7,10 @@ from skimage import transform, filters
 from augment import (img_augment, sample_img_augment_params, AugmentedInput,
                       SupervisedAugmentedInput)
 from util import img_transform
+
+def imgs(self):
+    for i in range(self.n_imgs):
+        yield self.img(i)
 def _resize(args):
     img, rescale_size, bbox = args
     img = img[bbox[0]:bbox[1], bbox[2]:bbox[3]]
@@ -14,7 +18,7 @@ def _resize(args):
     scale = img.shape[0] / float(rescale_size)
     sigma = np.sqrt(scale) / 2.0
     img = filters.gaussian_filter(img, sigma=sigma, multichannel=True)
-    img = transform.resize(img, (rescale_size, rescale_size, 3), order=3)
+    img = transform.resize(img, (rescale_size, rescale_size,3), order=3)
     img = (img*255).astype(np.uint8)
     return img
 
@@ -30,7 +34,11 @@ def _resize_augment(args):
     return img
 
 if __name__ == '__main__':
-	img=np.array(Image.open('./000001.jpg').convert('L'))
+#	img=np.array(Image.open('./000001.jpg').convert('L'))
+	img=np.array(Image.open('./000001.jpg'))
 	# a.show()
 	# a.save('aa.jpg')
-	_resize_augment((img,64,(40, 218-30, 15, 178-15)))
+	img = _resize_augment((img,64,(40, 218-30, 15, 178-15)))
+        image2 = Image.fromarray(img)
+	image2 = image2.convert('L')
+        image2.save('aa.jpg')
